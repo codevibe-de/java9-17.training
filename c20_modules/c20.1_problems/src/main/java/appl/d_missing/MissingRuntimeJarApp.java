1c2861205c98ef39eed5a900686d0f0fd4e163fd
Thomas Auinger <thomas@auinger.de>
2023-11-24 20:17:25 +0100

    fixed poms and added java call for d_missing

--- c20_modules/c20.1_problems/src/main/java/appl/d_missing/MissingRuntimeJarApp.java
+++ c20_modules/c20.1_problems/src/main/java/appl/d_missing/MissingRuntimeJarApp.java
@@ -1,4 +1,16 @@
 package appl.d_missing;
 
+import book.api.BookService;
+
+// a) `mvn clean install --projects c20.0_book,c20.1_problems` from `./c20_modules` dir
+// b) `java -cp c20.1_problems\target\c20.1_problems-1.0-SNAPSHOT.jar appl.d_missing.MissingRuntimeJarApp`
 public class MissingRuntimeJarApp {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Everything looking fine...");
+        Thread.sleep(2000);
+        System.out.println("Now accessing some class for the first time...");
+        Thread.sleep(3000);
+        BookService.INSTANCE.createBook("", "");
+    }
 }
