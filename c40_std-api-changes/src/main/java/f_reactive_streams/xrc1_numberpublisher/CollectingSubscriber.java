39a5ad938169bf6390904afbfde20619030e1324
Thomas Auinger <thomas@auinger.de>
2023-12-04 13:55:21 +0100

    added CollectingSubscr

---
+++ c40_std-api-changes/src/main/java/f_reactive_streams/xrc1_numberpublisher/CollectingSubscriber.java
@@ -0,0 +1,11 @@
+package f_reactive_streams.xrc1_numberpublisher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectingSubscriber {
+
+    private final List<Integer> collectedItems = new ArrayList<>();
+
+
+}
