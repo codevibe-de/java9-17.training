6f0fbed3e2d834ea26657858fc5f606e8471013e
Thomas Auinger <thomas@auinger.de>
2023-12-04 12:16:38 +0100

    minor imprvemts

--- c30_syntax/src/main/java/e_deprecated/DeprecatedApp.java
+++ c30_syntax/src/main/java/e_deprecated/DeprecatedApp.java
@@ -2,8 +2,6 @@ package e_deprecated;
 
 public class DeprecatedApp {
 
-    private static DeprecatedService service;
-
     public static void main(String[] args) {
         var service = new DeprecatedService();
         service.foo(); // compiles with warning
--- e30_syntax/src/main/java/book_converter/BookConverter.java
+++ e30_syntax/src/main/java/book_converter/BookConverter.java
@@ -5,6 +5,7 @@ public class BookConverter {
     public static final BookConverter INSTANCE = new BookConverter();
 
     public String convertBook(Book book, ContentType contentType) {
+        // todo implement this method
         return "todo";
     }
 
