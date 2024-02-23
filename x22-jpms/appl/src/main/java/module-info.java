d82ea0beb432f7063d5d54d4ce7fb37c99fe91c1
Thomas Auinger <thomas@auinger.de>
2024-02-23 09:43:21 +0100

    module books-report

--- x22-jpms/appl/src/main/java/module-info.java
+++ x22-jpms/appl/src/main/java/module-info.java
@@ -4,5 +4,6 @@ module x22_jpms.appl {
     requires org.fusesource.jansi;
 
     requires x22_jpms.books_core;
+    requires x22_jpms.books_report;
 
 }
---
+++ x22-jpms/books-report/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module x22_jpms.books_report {
+    requires x22_jpms.books_core;
+    exports book.io;
+}
