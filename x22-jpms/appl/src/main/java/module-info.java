f8d79a1097cb98d7d347f9ec7140545faad49fa6
Thomas Auinger <thomas@auinger.de>
2024-02-23 10:03:14 +0100

    requires transitive

--- x22-jpms/appl/src/main/java/module-info.java
+++ x22-jpms/appl/src/main/java/module-info.java
@@ -1,9 +1,5 @@
 module x22_jpms.appl {
-
     requires org.apache.commons.lang3;
     requires org.fusesource.jansi;
-
-    requires x22_jpms.books_core;
     requires x22_jpms.books_report;
-
 }
--- x22-jpms/books-report/src/main/java/module-info.java
+++ x22-jpms/books-report/src/main/java/module-info.java
@@ -1,4 +1,4 @@
 module x22_jpms.books_report {
-    requires x22_jpms.books_core;
+    requires transitive x22_jpms.books_core;
     exports book.io;
 }
