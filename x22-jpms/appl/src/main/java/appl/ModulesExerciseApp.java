018c53a5c76ad443eefb06582786565ba09771f7
Thomas Auinger <thomas@auinger.de>
2024-02-23 09:43:08 +0100

    refactored app

--- x22-jpms/appl/src/main/java/appl/ModulesExerciseApp.java
+++ x22-jpms/appl/src/main/java/appl/ModulesExerciseApp.java
@@ -3,35 +3,55 @@ package appl;
 import appl.reflection.Mapper;
 import book.api.Book;
 import book.api.Publisher;
+import book.io.BookReader;
+import book.io.BookReportWriter;
 import org.apache.commons.lang3.StringUtils;
 import org.fusesource.jansi.AnsiConsole;
+
+import java.util.List;
+import java.util.Objects;
 
 import static appl.jansi_ext.JansiColors.*;
 
 public class ModulesExerciseApp {
 
-    public static void main(String[] args) {
+    public ModulesExerciseApp() {
         AnsiConsole.systemInstall();
         cyan.println("\nApp starting...");
-        yellow.println(StringUtils.repeat('-', 80));
+    }
 
-        var book = createAndPrintBook();
+    public static void main(String[] args) {
+        // setup
+        var app = new ModulesExerciseApp();
+        printSeparator();
 
-        yellow.println(StringUtils.repeat('-', 80));
+        // work with books-core API
+        app.createAndPrintBook();
+        printSeparator();
 
-        investigate(book);
-        investigate(new Publisher("Random House"));
+        // use reflection with books-core classes
+        app.investigate(new Book("123-456", "Test", 2000, "The Duke"));
+        app.investigate(new Publisher("Sun Microsystems"));
 
+        // work with books-report API
+        printSeparator();
+        app.importBooksAndPrintReport();
+
+        // done
         System.out.println();
     }
 
-    private static Book createAndPrintBook() {
+    private static void printSeparator() {
+        yellow.println(StringUtils.repeat('-', 80));
+    }
+
+    private Book createAndPrintBook() {
         var book = new Book("0-19-501919-9", "A Pattern Language", 1977, "Christopher Alexander");
         System.out.println(book);
         return book;
     }
 
-    private static void investigate(Object instance) {
+    private void investigate(Object instance) {
         System.out.println("Fields of " + instance);
         var fields = Mapper.extractFields(instance);
         fields.entrySet().forEach(e -> {
@@ -39,6 +59,21 @@ public class ModulesExerciseApp {
             green.print(e.getKey());
             regular.println(": " + e.getValue());
         });
+    }
+
+    private void importBooksAndPrintReport() {
+        try {
+            String resourceName = "books.csv";
+            var in = Objects.requireNonNull(
+                    ClassLoader.getSystemResourceAsStream(resourceName),
+                    "Resource '%s' not found".formatted(resourceName)
+            );
+            List<Book> books = new BookReader().readBooksFromCsv(in);
+            String report = BookReportWriter.getStringInstance().generateReport(books);
+            cyan.println(report);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
