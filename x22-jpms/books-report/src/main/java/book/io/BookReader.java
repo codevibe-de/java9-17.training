3a6ecaa8e6caf06647d5646169a230c18cfce00f
Thomas Auinger <thomas@auinger.de>
2024-02-23 09:42:22 +0100

    refactored BooksReader and BooksReportWriter

--- x22-jpms/books-report/src/main/java/book/io/BookReader.java
+++ x22-jpms/books-report/src/main/java/book/io/BookReader.java
@@ -1,16 +1,17 @@
-package book;
+package book.io;
 
 import book.api.Book;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class BookReader {
 
-    public List<Book> readBooksFromCsv(Path csvFilePath) throws IOException {
-        return Files.readAllLines(csvFilePath).stream()
+    public List<Book> readBooksFromCsv(InputStream in) throws IOException {
+        var content = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+        return content.lines()
                 .map(l -> l.split(","))
                 .filter(parts -> parts.length == 4)
                 .map(parts -> new Book(parts[0], parts[1], Integer.parseInt(parts[2]), parts[3]))
