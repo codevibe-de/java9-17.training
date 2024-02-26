a20795f4489df195dc7bcad41dcadbd5e263f0a1
Thomas Auinger <thomas@auinger.de>
2024-02-26 08:20:53 +0100

    various c20 imprvmts

--- c20_modules/c20a_problems/src/main/java/appl/b_visibility/ProblemClassVisibilityApp.java
+++ c20_modules/c20a_problems/src/main/java/appl/b_visibility/ProblemClassVisibilityApp.java
@@ -1,12 +1,14 @@
 package appl.b_visibility;
 
+import book.DefaultBookService;
 import book.api.BookService;
 
 public class ProblemClassVisibilityApp {
 
     public static void main(String[] args) {
-        // we only want visibility to BookService, not its implementing classes
-        BookService bookService = BookService.INSTANCE;
+        // problem 3/5: we only want visibility to BookService, not its implementing classes
+        BookService bookService = new DefaultBookService();
+        bookService = BookService.INSTANCE; // doesn't help much
         bookService.createBook("123-345", "A Great Book");
     }
 
