0ad098c61c08819016b452fcf4cb5d8c3267b779
Thomas Auinger <thomas@auinger.de>
2024-07-23 14:09:09 +0200

    improved x30 book-converter code

--- x30_syntax/src/main/java/book_converter/BookConverterApp.java
+++ x30_syntax/src/main/java/book_converter/BookConverterApp.java
@@ -3,7 +3,7 @@ package book_converter;
 public class BookConverterApp {
 
     public static void main(String[] args) {
-        Book book = new Book("Java 9 Modularity", "Sander Mak");
+        Book book = new Book("Java 9 Modularity", "Sander Mak", 2017);
         System.out.println(
                 BookConverter.INSTANCE.convertBook(book, ContentType.XML)
         );
