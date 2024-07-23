bd432be7e3d494d3ef95bccaa8ffdddeea3509d6
Thomas Auinger <thomas@auinger.de>
2024-07-23 14:25:31 +0200

    sltn x30/CalculatorApi

--- x30_syntax/src/main/java/calculator/CalculatorApi.java
+++ x30_syntax/src/main/java/calculator/CalculatorApi.java
@@ -2,6 +2,10 @@ package calculator;
 
 public interface CalculatorApi {
 
+    /**
+     * @deprecated Use {@link #add(int, int)} or {@link #subtract(int, int)} instead.
+     */
+    @Deprecated(since = "1.1", forRemoval = true)
     default int calculate(int a, int b, String operator) {
         switch (operator) {
             case "+":
@@ -13,8 +17,25 @@ public interface CalculatorApi {
         }
     }
 
-    int add(int a, int b);
+    default int add(int a, int b) {
+        return calculateInternal(a, b, CalculatorOperation.ADD);
+    }
 
-    int subtract(int a, int b);
+    default int subtract(int a, int b) {
+        return calculateInternal(a, b, CalculatorOperation.SUBTRACT);
+    }
+
+    private int calculateInternal(int a, int b, CalculatorOperation calculatorOperation) {
+        var result = switch (calculatorOperation) {
+            case ADD -> a + b;
+            case SUBTRACT -> a - b;
+        };
+        System.Logger logger = System.getLogger(this.getClass().getName());
+        logger.log(
+                System.Logger.Level.DEBUG,
+                "%d %s %d = %d".formatted(a, calculatorOperation.getSymbol(), b, result)
+        );
+        return result;
+    }
 
 }
--- x30_syntax/src/main/java/calculator/CalculatorApp.java
+++ x30_syntax/src/main/java/calculator/CalculatorApp.java
@@ -3,6 +3,9 @@ package calculator;
 public class CalculatorApp {
 
     public static void main(String[] args) {
-
+        var calculator = new CalculatorApi() {
+        };
+        calculator.add(1, 2);
+        calculator.subtract(1, 2);
     }
 }
