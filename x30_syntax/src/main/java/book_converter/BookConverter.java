6498a70baf474ff86d599afbab9183d380c04b4e
Thomas Auinger <thomas@auinger.de>
2024-07-23 13:50:35 +0200

    sltn x30/BookConverter

--- x30_syntax/src/main/java/book_converter/BookConverter.java
+++ x30_syntax/src/main/java/book_converter/BookConverter.java
@@ -5,8 +5,40 @@ public class BookConverter {
     public static final BookConverter INSTANCE = new BookConverter();
 
     public String convertBook(Book book, ContentType contentType) {
-        // todo implement this method
-        return "todo";
+        return switch (contentType) {
+            case XML -> convertToXml(book);
+            case JSON -> convertToJson(book);
+            case PLAIN_TEXT -> convertToPlainText(book);
+        };
+    }
+
+    private String convertToXml(Book book) {
+        return """
+                <book>
+                    <title>%s</title>
+                    <author>%s</author>
+                    <year>%4d</year>
+                </book>
+                """.formatted(book.getTitle(), book.getAuthor(), book.getYear());
+    }
+
+    private String convertToJson(Book book) {
+        return """
+                {
+                    "title": "%s",
+                    "author": "%s",
+                    "year": %4d
+                }
+                """.formatted(book.getTitle(), book.getAuthor(), book.getYear());
+    }
+
+    private String convertToPlainText(Book book) {
+        return """
+                %s
+                - Author: %s
+                - Year: %4d
+                """.formatted(book.getTitle(), book.getAuthor(), book.getYear());
+
     }
 
 }
