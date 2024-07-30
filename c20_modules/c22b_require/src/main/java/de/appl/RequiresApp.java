a5a417ce62d3586dc9ea47e8434edd6a27085b05
Thomas Auinger <thomas@auinger.de>
2024-07-30 10:58:38 +0200

    refactored main()

--- c20_modules/c22b_require/src/main/java/de/appl/RequiresApp.java
+++ c20_modules/c22b_require/src/main/java/de/appl/RequiresApp.java
@@ -3,19 +3,9 @@ package de.appl;
 import de.book.api.BookService;
 import org.assertj.core.api.Assertions;
 
-import java.lang.reflect.InvocationTargetException;
-
-import static utils.MethodLogger.logMethodCall;
-
 public class RequiresApp {
 
-    public static void main(String[] args) throws Exception {
-        demoBookServiceAccess();
-    }
-
-    static void demoBookServiceAccess() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        logMethodCall();
-
+    public static void main(String[] args) {
         BookService bookService = BookService.INSTANCE;
         var book = bookService.addBook("Title", "Author", null, null);
         System.out.println(book);
